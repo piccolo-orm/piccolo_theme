70d18abbb1d9caf3c9994156f919d64de6a89f3b
Daniel Townsend <dan@dantownsend.co.uk>
2022-02-12 16:55:38 +0000

    style and format autodoc output

--- piccolo_theme/static/js/theme.js
+++ piccolo_theme/static/js/theme.js
@@ -1,1 +1,19 @@
-console.log("custom theme loaded")
+window.onload = function() {
+    console.log("custom theme loaded")
+
+    const paramElements = document.getElementsByClassName('sig-param')
+
+    Array(...paramElements).forEach((element) => {
+        console.log('adding element ...')
+        let brElement = document.createElement('br');
+        element.parentNode.insertBefore(brElement, element)
+    })
+
+    const lastParamElements = document.querySelectorAll('em.sig-param:last-of-type')
+
+    Array(...lastParamElements).forEach((element) => {
+        console.log('adding end element ...')
+        let brElement = document.createElement('br');
+        element.after(brElement)
+    })
+}
