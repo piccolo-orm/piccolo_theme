41ec831d05eec18b02d5e1df663aa56d4779afde
Daniel Townsend <dan@dantownsend.co.uk>
2022-02-12 21:50:25 +0000

    styled searchbar

--- piccolo_theme/static/js/theme.js
+++ piccolo_theme/static/js/theme.js
@@ -1,11 +1,14 @@
-window.onload = function() {
-    console.log("custom theme loaded")
 
+/**
+ * We add extra br tags to the autodoc output, so each parameter is shown on
+ * its own line.
+ */
+function setupAutodoc() {
     const paramElements = document.getElementsByClassName('sig-param')
 
     Array(...paramElements).forEach((element) => {
         console.log('adding element ...')
-        let brElement = document.createElement('br');
+        let brElement = document.createElement('br')
         element.parentNode.insertBefore(brElement, element)
     })
 
@@ -13,7 +16,20 @@ window.onload = function() {
 
     Array(...lastParamElements).forEach((element) => {
         console.log('adding end element ...')
-        let brElement = document.createElement('br');
+        let brElement = document.createElement('br')
         element.after(brElement)
     })
+}
+
+function setupSearchSidebar() {
+    document.querySelector('form.search input[type=text]').placeholder = 'Search...'
+
+    document.querySelector('form.search input[type=submit]').value = 'Search'
+}
+
+window.onload = function() {
+    console.log("custom theme loaded")
+
+    setupAutodoc()
+    setupSearchSidebar()
 }
